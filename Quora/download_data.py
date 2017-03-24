0bf222820b2823d12ff13bd61ffe5e7121309c69
Evan Oman <evan058@gmail.com>
2017-03-23 23:47:25 -0500

    Finished :fire: impl, added comments

--- Quora/download_data.py
+++ Quora/download_data.py
@@ -4,30 +4,48 @@ import requests, os, sys, shutil, zipfile, fire
 import ssl
 
 class Download_Data(object): 
-    def dl_all(uname, passw, is_clean = "F"):
-        if is_clean is "T":
+    def dl_all(self, uname, passw, optCmd):
+
+        # Clean the data prompts if prompted
+        if optCmd is "clean":
                 print("Clean: removing ./dat folder\n\n")
                 shutil.rmtree("./dat")
 
+        # if the data directory exists, do nothing
         if (os.path.isdir('./dat')):
             print("Data already downloaded")
+
+        # otherwise download teh dataz
         else:
+
+            # website specifics
+            # TODO: this really could be refactored into a function which accepts these as params
             baseURL = "https://www.kaggle.com/c/quora-question-pairs/download/"
             fNames = ["test.csv.zip", "train.csv.zip"]
             
             # make the dir
             os.makedirs("dat")
             
+            # kaggle user account info 
             kaggle_info = {'UserName': uname, 'Password': passw}
+
+            # download and extract each file
             for fileName in fNames:
+
                 print("##############")
+                
                 # Attempts to download the CSV file. Gets rejected because we are not logged in.
                 r = requests.get(baseURL + fileName)
+                
                 print("Downloading: " + fileName)
+                
                 # Login to Kaggle and retrieve the data.
                 r = requests.post(r.url, data = kaggle_info)
+                
                 # Writes the data to a local file one chunk at a time.
                 f = open("./dat/" + fileName, 'wb')
+                
+                # write each chunk to the file
                 for chunk in r.iter_content(chunk_size = 512 * 1024): # Reads 512KB at a time into memory
                     if chunk: # filter out keep-alive new chunks
                         f.write(chunk)
@@ -45,4 +63,5 @@ class Download_Data(object):
                 print("##############")
 
 if (__name__ == "__main__"):
+    # perform google fire magic
     fire.Fire(Download_Data)
